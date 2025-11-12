519b71e91ca750663c9a23a1e69ec412b4e7e27a
BOOK-0U7NTJSUB8\linar <linards.jans@gmail.com>
2025-11-13 00:46:40 +0100

    parallax bg

--- The-Lost-Elements/Assets/Scripts/Player/ability.cs
+++ The-Lost-Elements/Assets/Scripts/Player/ability.cs
@@ -7,27 +7,23 @@ public class SimpleSpellCombiner : MonoBehaviour
 
     void Update()
     {
-        // Press 1 to collect Fire
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             hasFire = true;
             Debug.Log("Fire selected");
         }
 
-        // Press 2 to collect Water
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             hasWater = true;
             Debug.Log("Water selected");
         }
 
-        // Press 3 to combine
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             if (hasFire && hasWater)
             {
                 Debug.Log("🔥💧 Combined into Steam Burst!");
-                // Spawn your ability prefab here later
                 hasFire = false;
                 hasWater = false;
             }
